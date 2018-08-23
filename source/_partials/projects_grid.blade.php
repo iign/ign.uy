829773da8003a470ad1c4fc0da3dc1a95d87f1d9
Ignacio Toledo <ignacio@ign.uy>
2018-08-23 12:26:03 -0300

    Refactor, stop versioning unnecessary files.

---
+++ source/_partials/projects_grid.blade.php
@@ -0,0 +1,90 @@
+<section class="section-projects">
+  <div class="container">
+    <div class="projects-grid">
+
+      <a href="/project/givebutter" class="project">
+        <img class="project__img" src="/uploads/GiveButter.jpg" srcset="/uploads/GiveButter.jpg 1x" alt="GiveButter">
+        <div class="project__overlay">
+          <div class="project__overlay-content">
+            <h3 class="project__title">
+              GiveButter
+            </h3>
+            <h4 class="project__cat">Web Markup</h4>
+          </div>
+        </div>
+      </a>
+
+      <a href="/project/mus" class="project">
+        <img class="project__img" src="/uploads/mus.jpg" srcset="/uploads/mus.jpg 1x, /uploads/mus@2x.jpg 2x" alt="Mus App">
+        <div class="project__overlay">
+          <div class="project__overlay-content">
+            <h3 class="project__title">
+              Mus web App
+            </h3>
+            <h4 class="project__cat">Web Markup</h4>
+          </div>
+        </div>
+      </a>
+
+      <a href="/project/safehouse" class="project">
+        <img class="project__img" src="/uploads/safehouse.jpg" srcset="/uploads/safehouse.jpg 1x, /uploads/safehouse@2x.jpg 2x" alt="SafeHouse">
+        <div class="project__overlay">
+          <div class="project__overlay-content">
+            <h3 class="project__title">
+              SafeHouse
+            </h3>
+            <h4 class="project__cat">Mobile design</h4>
+          </div>
+        </div>
+      </a>
+
+      <a href="/project/passgen" class="project">
+        <img class="project__img" src="/uploads/passgen.jpg" srcset="/uploads/passgen.jpg 1x, /uploads/passgen@2x.jpg 2x" alt="PassGen">
+        <div class="project__overlay">
+          <div class="project__overlay-content">
+            <h3 class="project__title">
+              PassGen
+            </h3>
+            <h4 class="project__cat">UX/UI, Markup, Website</h4>
+          </div>
+        </div>
+      </a>
+
+      <a href="/project/nahual" class="project">
+        <img class="project__img" src="/uploads/nahual.jpg" alt="Nahual Project">
+        <div class="project__overlay">
+          <div class="project__overlay-content">
+            <h3 class="project__title">
+              Proyecto Nahual
+            </h3>
+            <h4 class="project__cat">Logo design</h4>
+          </div>
+        </div>
+      </a>
+      <a href="/project/flying-cargo" class="project">
+        <img class="project__img" src="/uploads/flying-cargo.jpg" srcset="/uploads/flying-cargo.jpg 1x, /uploads/flying-cargo@2x.jpg 2x" alt="Flying Cargo">
+        <div class="project__overlay">
+          <div class="project__overlay-content">
+            <h3 class="project__title">
+              Flying Cargo
+            </h3>
+            <h4 class="project__cat">Web Design</h4>
+          </div>
+        </div>
+      </a>
+
+      <a href="/project/nugen" class="project">
+        <img class="project__img" src="/uploads/nugen.jpg" srcset="/uploads/nugen.jpg 1x, /uploads/nugen@2x.jpg 2x" alt="NuGen">
+        <div class="project__overlay">
+          <div class="project__overlay-content">
+            <h3 class="project__title">
+              NuGen
+            </h3>
+            <h4 class="project__cat">Logo design</h4>
+          </div>
+        </div>
+      </a>
+
+    </div>
+  </div>
+</section>
