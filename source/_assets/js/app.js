b405abd36733c4b80cf3fb27ae03879e89795238
Ignacio Toledo <ignacio@ign.uy>
2018-08-29 15:30:25 -0300

    Removed unused js

--- source/_assets/js/app.js
+++ source/_assets/js/app.js
@@ -1,18 +1,7 @@
-let el = document.querySelector('.js-more')
-if (el) {
-  el.addEventListener('click', function () {
-    showMore()
-  }, false)
-}
-
-function showMore () {
-  document.querySelector('.intro').classList.add('intro--active')
-}
-
 function sayHi () {
   if (!document.querySelector('.hallo')) { return }
   let hallo = document.querySelector('.hallo')
-  let items = ['Hallo', 'Hola', '👋', 'Hey']
+  let items = ['Hallo', 'Hola', '👋', 'Hi']
   let item = items[Math.floor(Math.random() * items.length)]
   hallo.innerHTML = item
 }
