ca65d5edf529449b08868c189e948f0d60e2d463
Ignacio Toledo <ignacio@ign.uy>
2020-04-21 20:13:48 -0300

    Style update

--- source/_layouts/project.blade.php
+++ source/_layouts/project.blade.php
@@ -1,29 +1,31 @@
 <!doctype html>
 <html lang="en">
-  @include('_partials.head')
-  <body>
-    @include('_partials.header')
+@include('_partials.head')
 
-    <div class="project-detail__title">
-        <div class="container">
-            @yield('project_name')
-        </div>
+<body>
+  @include('_partials.header')
+
+  <div class="container">
+    <h1 class="project-detail__title">
+      @yield('project_name')
+    </h1>
+  </div>
+
+  <div class="project-detail__intro">
+    <div class="container">
+      @yield('project_intro')
     </div>
+  </div>
 
-    <div class="project-detail__intro">
-      <div class="container">
-        @yield('project_intro')
-      </div>
+  <section class="section-project">
+    <div class="container">
+      @yield('project_body')
     </div>
+  </section>
 
-    <section class="section-project">
-      <div class="container">
-        @yield('project_body')
-      </div>
-    </section>
-
-    @include('_partials.footer')
+  @include('_partials.footer')
 
 
-  </body>
+</body>
+
 </html>
