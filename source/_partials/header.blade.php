2914fbe005be232113b54e43cf61d5b6a12c202e
Ignacio Toledo <ignacio@ign.uy>
2020-04-21 20:39:48 -0300

    Improve a11y

--- source/_partials/header.blade.php
+++ source/_partials/header.blade.php
@@ -1,13 +1,9 @@
 <header class="container header" id="top">
   <a href="/" class="top-name">@yield('top_link', 'Nacho Toledo')</a>
   <div class="contact-links">
-    <div class="contact-links__col">
-      <a class="contact-link" href="mailto:hi@ign.uy">Email</a>
-      <a class="contact-link" href="http://blog.ign.uy">Blog</a>
-    </div>
-    <div class="contact-links__col">
-      <a class="contact-link" href="https://github.com/iign">GitHub</a>
-      <a class="contact-link" href="https://dribbble.com/iign">Dribbble</a>
-    </div>
+    <a class="contact-link" href="mailto:hi@ign.uy">Email</a>
+    <a class="contact-link" href="http://blog.ign.uy">Blog</a>
+    <a class="contact-link" href="https://github.com/iign">GitHub</a>
+    <a class="contact-link" href="https://dribbble.com/iign">Dribbble</a>
   </div>
 </header>
