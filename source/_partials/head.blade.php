0e2653e9c54353035f4e5dfd78d1552b409be2ab
Ignacio Toledo <ignacio@ign.uy>
2020-04-21 21:05:33 -0300

    Improve perf

--- source/_partials/head.blade.php
+++ source/_partials/head.blade.php
@@ -22,7 +22,6 @@
   <title>@yield('title')</title>
   <meta name="description" content="@yield('description')">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://fonts.googleapis.com/css?family=DM+Sans:400,400i,500,700&display=swap" rel="stylesheet">
   <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
@@ -31,4 +30,19 @@
   <meta name="msapplication-TileColor" content="#000000">
   <meta name="theme-color" content="#000000">
   <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+  <script>
+    function sayHi() {
+      if (!document.querySelector('.hallo')) {
+        return
+      }
+      let hallo = document.querySelector('.hallo')
+      let items = ['Hallo', 'Hola', '👋', 'Hi']
+      let item = items[Math.floor(Math.random() * items.length)]
+      hallo.innerHTML = item
+    }
+
+    document.addEventListener('DOMContentLoaded', function(event) {
+      sayHi()
+    })
+  </script>
 </head>
