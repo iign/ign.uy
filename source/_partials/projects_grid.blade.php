a71464b6b3ab51a8d75028e212ad50d58f3da508
Ignacio Toledo <ignacio@ign.uy>
2021-01-12 20:44:18 -0300

    Added stylespring, removed old projects

--- source/_partials/projects_grid.blade.php
+++ source/_partials/projects_grid.blade.php
@@ -1,149 +1,149 @@
 <section class="section-projects">
-  <div class="container">
-    <div class="projects-grid">
+    <div class="container">
+        <div class="projects-grid">
 
-      <a href="/project/gcm" class="project">
-        <picture class="project__img">
-          <source srcset="/uploads/gcm-thumb.webp" type="image/webp">
-          <img src="/uploads/gcm.jpg" srcset="/uploads/gcm-thumb.jpg 1x" alt="Global Commerce Media">
-        </picture>
+            <a href="/project/stylespring" class="project">
+                <picture class="project__img">
+                    <source srcset="/uploads/stylespring-thumb.webp" type="image/webp">
+                    <img src="/uploads/stylespring-thumb.jpg" alt="Stylespring">
+                </picture>
 
-        <div class="project__overlay">
-          <div class="project__overlay-content">
-            <h3 class="project__title">
-              Global Commerce Media
-            </h3>
-            <h4 class="project__cat">Landing page</h4>
-          </div>
+                <div class="project__overlay">
+                    <div class="project__overlay-content">
+                        <h3 class="project__title">
+                            Stylespring
+                        </h3>
+                        <h4 class="project__cat">Web markup</h4>
+                    </div>
+                </div>
+            </a>
+
+            <a href="/project/gcm" class="project">
+                <picture class="project__img">
+                    <source srcset="/uploads/gcm-thumb.webp" type="image/webp">
+                    <img src="/uploads/gcm-thumb.jpg" alt="Global Commerce Media">
+                </picture>
+
+                <div class="project__overlay">
+                    <div class="project__overlay-content">
+                        <h3 class="project__title">
+                            Global Commerce Media
+                        </h3>
+                        <h4 class="project__cat">Landing page</h4>
+                    </div>
+                </div>
+            </a>
+
+            <a href="/project/asg" class="project">
+                <picture class="project__img">
+                    <source srcset="/uploads/asg.webp" type="image/webp">
+                    <img src="/uploads/asg.jpg" srcset="/uploads/asg.jpg 1x" alt="Archsourcing">
+                </picture>
+
+                <div class="project__overlay">
+                    <div class="project__overlay-content">
+                        <h3 class="project__title">
+                            /ARCHSOURCING
+                        </h3>
+                        <h4 class="project__cat">Single page site</h4>
+                    </div>
+                </div>
+            </a>
+
+            <a href="/project/givebutter" class="project">
+                <picture class="project__img">
+                    <source srcset="/uploads/GiveButter.webp" type="image/webp">
+                    <img src="/uploads/GiveButter.jpg" srcset="/uploads/GiveButter.jpg 1x" alt="GiveButter">
+                </picture>
+
+                <div class="project__overlay">
+                    <div class="project__overlay-content">
+                        <h3 class="project__title">
+                            GiveButter
+                        </h3>
+                        <h4 class="project__cat">Web Markup</h4>
+                    </div>
+                </div>
+            </a>
+
+            <a href="/project/logo-design" class="project">
+                <picture class="project__img">
+                    <source srcset="/uploads/logo-design.webp" type="image/webp">
+                    <img src="/uploads/logo-design.jpg" srcset="/uploads/logo-design.jpg 1x, /uploads/logo-design@2x.jpg 2x" alt="Logo design & identity">
+                </picture>
+
+                <div class="project__overlay">
+                    <div class="project__overlay-content">
+                        <h3 class="project__title">
+                            Logo design
+                        </h3>
+                        <h4 class="project__cat">Design &amp; Identity</h4>
+                    </div>
+                </div>
+            </a>
+
+            <a href="/project/mus" class="project">
+                <picture class="project__img">
+                    <source srcset="/uploads/mus.webp" type="image/webp">
+                    <img src="/uploads/mus.jpg" srcset="/uploads/mus.jpg 1x, /uploads/mus@2x.jpg 2x" alt="Mus App">
+                </picture>
+
+                <div class="project__overlay">
+                    <div class="project__overlay-content">
+                        <h3 class="project__title">
+                            Web player app
+                        </h3>
+                        <h4 class="project__cat">Web Markup, landing page</h4>
+                    </div>
+                </div>
+            </a>
+
+            <a href="/project/safehouse" class="project">
+                <picture class="project__img">
+                    <source srcset="/uploads/safehouse.webp" type="image/webp">
+                    <img src="/uploads/safehouse.jpg" srcset="/uploads/safehouse.jpg 1x, /uploads/safehouse@2x.jpg 2x" alt="SafeHouse">
+                </picture>
+                <div class="project__overlay">
+                    <div class="project__overlay-content">
+                        <h3 class="project__title">
+                            SafeHouse
+                        </h3>
+                        <h4 class="project__cat">Mobile design</h4>
+                    </div>
+                </div>
+            </a>
+
+            <a href="/project/passgen" class="project">
+                <picture class="project__img">
+                    <source srcset="/uploads/passgen.webp" type="image/webp">
+                    <img src="/uploads/passgen.jpg" srcset="/uploads/passgen.jpg 1x, /uploads/passgen@2x.jpg 2x" alt="PassGen">
+                </picture>
+
+                <div class="project__overlay">
+                    <div class="project__overlay-content">
+                        <h3 class="project__title">
+                            PassGen
+                        </h3>
+                        <h4 class="project__cat">UX/UI, Markup, Website</h4>
+                    </div>
+                </div>
+            </a>
+
+            <a href="/project/nahual" class="project">
+                <picture class="project__img">
+                    <source srcset="/uploads/nahual.webp" type="image/webp">
+                    <img src="/uploads/nahual.jpg" alt="Nahual Project">
+                </picture>
+                <div class="project__overlay">
+                    <div class="project__overlay-content">
+                        <h3 class="project__title">
+                            Proyecto Nahual
+                        </h3>
+                        <h4 class="project__cat">Logo design</h4>
+                    </div>
+                </div>
+            </a>
+
         </div>
-      </a>
-
-      <a href="/project/asg" class="project">
-        <picture class="project__img">
-          <source srcset="/uploads/asg.webp" type="image/webp">
-          <img src="/uploads/asg.jpg" srcset="/uploads/asg.jpg 1x" alt="Archsourcing">
-        </picture>
-
-        <div class="project__overlay">
-          <div class="project__overlay-content">
-            <h3 class="project__title">
-              /ARCHSOURCING
-            </h3>
-            <h4 class="project__cat">Single page site</h4>
-          </div>
-        </div>
-      </a>
-
-      <a href="/project/givebutter" class="project">
-        <picture class="project__img">
-          <source srcset="/uploads/GiveButter.webp" type="image/webp">
-          <img src="/uploads/GiveButter.jpg" srcset="/uploads/GiveButter.jpg 1x" alt="GiveButter">
-        </picture>
-
-        <div class="project__overlay">
-          <div class="project__overlay-content">
-            <h3 class="project__title">
-              GiveButter
-            </h3>
-            <h4 class="project__cat">Web Markup</h4>
-          </div>
-        </div>
-      </a>
-
-      <a href="/project/logo-design" class="project">
-        <picture class="project__img">
-          <source srcset="/uploads/logo-design.webp" type="image/webp">
-          <img src="/uploads/logo-design.jpg" srcset="/uploads/logo-design.jpg 1x, /uploads/logo-design@2x.jpg 2x" alt="Logo design & identity">
-        </picture>
-
-        <div class="project__overlay">
-          <div class="project__overlay-content">
-            <h3 class="project__title">
-              Logo design
-            </h3>
-            <h4 class="project__cat">Design &amp; Identity</h4>
-          </div>
-        </div>
-      </a>
-
-      <a href="/project/mus" class="project">
-        <picture class="project__img">
-          <source srcset="/uploads/mus.webp" type="image/webp">
-          <img src="/uploads/mus.jpg" srcset="/uploads/mus.jpg 1x, /uploads/mus@2x.jpg 2x" alt="Mus App">
-        </picture>
-
-        <div class="project__overlay">
-          <div class="project__overlay-content">
-            <h3 class="project__title">
-              Web player app
-            </h3>
-            <h4 class="project__cat">Web Markup, landing page</h4>
-          </div>
-        </div>
-      </a>
-
-      <a href="/project/safehouse" class="project">
-        <picture class="project__img">
-          <source srcset="/uploads/safehouse.webp" type="image/webp">
-          <img src="/uploads/safehouse.jpg" srcset="/uploads/safehouse.jpg 1x, /uploads/safehouse@2x.jpg 2x" alt="SafeHouse">
-        </picture>
-        <div class="project__overlay">
-          <div class="project__overlay-content">
-            <h3 class="project__title">
-              SafeHouse
-            </h3>
-            <h4 class="project__cat">Mobile design</h4>
-          </div>
-        </div>
-      </a>
-
-      <a href="/project/passgen" class="project">
-        <picture class="project__img">
-          <source srcset="/uploads/passgen.webp" type="image/webp">
-          <img src="/uploads/passgen.jpg" srcset="/uploads/passgen.jpg 1x, /uploads/passgen@2x.jpg 2x" alt="PassGen">
-        </picture>
-
-        <div class="project__overlay">
-          <div class="project__overlay-content">
-            <h3 class="project__title">
-              PassGen
-            </h3>
-            <h4 class="project__cat">UX/UI, Markup, Website</h4>
-          </div>
-        </div>
-      </a>
-
-      <a href="/project/nahual" class="project">
-        <picture class="project__img">
-          <source srcset="/uploads/nahual.webp" type="image/webp">
-          <img src="/uploads/nahual.jpg" alt="Nahual Project">
-        </picture>
-        <div class="project__overlay">
-          <div class="project__overlay-content">
-            <h3 class="project__title">
-              Proyecto Nahual
-            </h3>
-            <h4 class="project__cat">Logo design</h4>
-          </div>
-        </div>
-      </a>
-
-      <a href="/project/nugen" class="project">
-        <picture class="project__img">
-          <source srcset="/uploads/nugen.webp" type="image/webp">
-          <img src="/uploads/nugen.jpg" srcset="/uploads/nugen.jpg 1x, /uploads/nugen@2x.jpg 2x" alt="NuGen">
-        </picture>
-
-        <div class="project__overlay">
-          <div class="project__overlay-content">
-            <h3 class="project__title">
-              NuGen
-            </h3>
-            <h4 class="project__cat">Logo design</h4>
-          </div>
-        </div>
-      </a>
-
     </div>
-  </div>
 </section>
---
+++ source/project/stylespring.blade.php
@@ -0,0 +1,38 @@
+@extends('_layouts.project')
+@section('title', 'Stylespring')
+@section('description', 'Stylespring landing page')
+@section('top_link', '← Back')
+@section('project_name', 'Stylespring')
+
+@section('body')
+
+@section('project_name')
+Stylespring
+@endsection
+
+@section('project_intro')
+<p>
+    Web markup for <em>Stylespring</em>, a fashion products aggregator made by Global Commerce Media.
+    <br>
+    Mobile-first, responsive web markup.
+</p>
+
+<p>
+    Explore the demo page: <a href="https://stylespring.netlify.app/">Stylespring homepage</a>
+</p>
+
+@endsection
+
+@section('project_body')
+
+<img class="project-detail__img img-shadow" src="/uploads/stylespring/Stylespring_Desktop_Home.jpg" srcset="/uploads/stylespring/Stylespring_Desktop_Home.jpg 1x, /uploads/Stylespring_Desktop_Home/stylespring@2x.jpg 2x" alt="Stylespring homepage">
+
+<img class="project-detail__img img-shadow" src="/uploads/stylespring/Stylespring_Desktop_Article.jpg" srcset="/uploads/stylespring/Stylespring_Desktop_Article.jpg 1x, /uploads/Stylespring_Desktop_Article/stylespring@2x.jpg 2x" alt="Stylespring homepage">
+
+
+
+
+
+@endsection
+
+@endsection
